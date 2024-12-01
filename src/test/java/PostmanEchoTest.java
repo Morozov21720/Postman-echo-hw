b8592b9f3c1f64569405906d1b7b4025a734f223
MorozovIvan <morozov21720@mail.ru>
2024-12-01 17:21:37 +0500

    correct data test

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -14,6 +14,6 @@ class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("wrong data")); // Проверка содержимого
+                .body("data", equalTo("some data")); // Проверка содержимого
     }
 }
